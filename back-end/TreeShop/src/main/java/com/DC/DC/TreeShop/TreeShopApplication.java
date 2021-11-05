4905994a11cede9073671d45da736918e08706de
Dănilă Daniel <daniladanielmarian@gmail.com>
2021-11-05 22:07:53 +0200

    Spring folder

---
+++ back-end/TreeShop/src/main/java/com/DC/DC/TreeShop/TreeShopApplication.java
@@ -0,0 +1,13 @@
+package com.DC.DC.TreeShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeShopApplication.class, args);
+	}
+
+}
---
+++ back-end/TreeShop/src/test/java/com/DC/DC/TreeShop/TreeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DC.DC.TreeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
